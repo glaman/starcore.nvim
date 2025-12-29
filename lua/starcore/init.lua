e2a2c9a07b410c3a1a716c6102dc15706d033935
Clint <jarbs1605@gmail.com>
2025-12-28 22:04:07 -0500

    which key support added

--- lua/starcore/init.lua
+++ lua/starcore/init.lua
@@ -9,6 +9,10 @@ function M.setup(opts)
 	if package.loaded["telescope"] then
 		require("starcode.integrations.telescope").setup()
 	end
+  pcall(require, "which-key")
+  if package.loaded["which-key"] then
+    require("starcore.integrations.which-key").setup()
+  end
 end
 
 return M
